d17de5483afd66f32698b50627b91b7013da1263
azhar <azharjaveedpz@gmail.com>
2025-07-12 03:15:48 +0530

    added page factory

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -1,6 +1,5 @@
 package pages;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 import utils.TestDataReader;
@@ -8,47 +7,60 @@ import utils.TestDataReader;
 import java.util.NoSuchElementException;
 import java.util.Properties;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+
+
 public class LoginPage {
     private WebDriver driver;
     private Properties prop;
-
-    // Locators
-    private By emailField = By.id("username");
-    private By passwordField = By.id("password");
-    private By loginButton = By.id("btn_login");
-    private By loginErrorMessage = By.xpath("//em[normalize-space()='An incorrect Username or Password was specified']"); 
 
     // Constructor with config passed from BaseTest
     public LoginPage(WebDriver driver, Properties prop) {
         this.driver = driver;
         this.prop = prop;
+        PageFactory.initElements(driver, this);
     }
+
+    // WebElements using @FindBy
+    @FindBy(id = "username")
+    private WebElement emailField;
+
+    @FindBy(id = "password")
+    private WebElement passwordField;
+
+    @FindBy(id = "btn_login")
+    private WebElement loginButton;
+
+    @FindBy(xpath = "//em[normalize-space()='An incorrect Username or Password was specified']")
+    private WebElement loginErrorMessage;
 
     // Actions
     public void enterEmail(String email) {
-        driver.findElement(emailField).sendKeys(email);
+        emailField.sendKeys(email);
     }
 
     public void enterPassword(String password) {
-        driver.findElement(passwordField).sendKeys(password);
+        passwordField.sendKeys(password);
     }
 
     public void clickLogin() {
-        driver.findElement(loginButton).click();
+        loginButton.click();
     }
-    
+
     public String getLoginErrorMessage() {
         try {
-            return driver.findElement(loginErrorMessage).getText().trim();
+            return loginErrorMessage.getText().trim();
         } catch (NoSuchElementException e) {
             return "";
         }
     }
 
-
     // Full login with data loaded from login.properties
     public void login() {
-        Properties loginData = TestDataReader.load("login","password", prop);
+        Properties loginData = TestDataReader.load("login", "password", prop);
         String username = loginData.getProperty("username");
         String password = loginData.getProperty("password");
 
@@ -56,7 +68,7 @@ public class LoginPage {
         enterPassword(password);
         clickLogin();
     }
-    
+
     public boolean loginErrMessageValidation() {
         String errorMsg = getLoginErrorMessage();
         return !errorMsg.isEmpty(); // returns true if there's a message
