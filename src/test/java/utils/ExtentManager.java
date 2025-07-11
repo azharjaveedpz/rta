e00370240bf5ef3459f4de034b7c5d92a5c2671f
azhar <azharjaveedpz@gmail.com>
2025-07-12 03:49:58 +0530

    added failed screenshot

--- src/test/java/utils/ExtentManager.java
+++ src/test/java/utils/ExtentManager.java
@@ -5,6 +5,7 @@ import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 
+import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -20,7 +21,10 @@ public class ExtentManager {
 
     private static void createInstance() {
         String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String reportPath = System.getProperty("user.dir") + "/reports/TestReport_" + timestamp + ".html";
+        String reportsDir = System.getProperty("user.dir") + "/reports";
+        new File(reportsDir).mkdirs(); // Ensure folder exists
+
+        String reportPath = reportsDir + "/TestReport_" + timestamp + ".html";
 
         ExtentSparkReporter sparkReporter = new ExtentSparkReporter(reportPath);
         sparkReporter.config().setReportName("Automation Test Report");
@@ -29,4 +33,5 @@ public class ExtentManager {
         extent = new ExtentReports();
         extent.attachReporter(sparkReporter);
     }
+
 }
